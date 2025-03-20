d98ba5d9b5f2a267fcc890df9f20d5c91913cb9e
OfficialPIRAX <cedricgmx@icloud.com>
2025-03-20 01:20:58 +0100

    Added icon.

--- vite.config.js
+++ vite.config.js
@@ -9,6 +9,11 @@ export default defineConfig({
     outDir: 'dist',
     assetsDir: 'assets',
     emptyOutDir: true,
+    rollupOptions: {
+      input: {
+        main: path.resolve(__dirname, 'index.html'),
+      },
+    },
   },
   resolve: {
     alias: {
